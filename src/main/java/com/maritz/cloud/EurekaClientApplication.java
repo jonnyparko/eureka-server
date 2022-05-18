49aa7a2e14d0d583038ddf9712bd24fcfb2de616
PARKERJ0 <jonathan.parker@maritz.com>
2022-05-18 15:11:42 -0500

    first commit

---
+++ src/main/java/com/maritz/cloud/EurekaClientApplication.java
@@ -0,0 +1,15 @@
+package com.maritz.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maritz/cloud/EurekaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maritz.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
